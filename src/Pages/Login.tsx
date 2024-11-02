91dc85baabe47d3832575d7dae99169908cd1184
Lucas Moura <lucasmoura53@gmail.com>
2024-11-02 18:40:59 -0300

    atual1

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
+import {styles} from '../Styles'
+
+
+export function Login(){
+    return(
+        <View style={styles.container}>
+            <Text>Login</Text>
+        </View>
+    )
+}
---
+++ src/Pages/Signin.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
+import {styles} from '../Styles'
+
+
+export function Signin(){
+    return(
+        <View style={styles.container}>
+            <Text>Signin
+            
+            </Text>
+        </View>
+    )
+}
